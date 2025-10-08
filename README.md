d979abaa7f9410378dfd439348f58e01398709e0
asco0145 <asco0145@uni.sydney.edu.au>
2025-10-08 21:49:00 +1100

    Quiz 8

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
-# idea9103_test
-This is a test for the IDEA 9103 Class XD !!
+# Quiz 8 
+This is my quiz 8 submission.
 
-This is the read me file C:
+### **Part 1**
+Glitch art is an unconventional style that transforms traditional imagery into something unpredictable and striking. It can be achieved through databending, circuit bending, or direct data manipulation. I find this inspiring because the outcomes are endless—no two pieces are alike, as results depend on how the program is coded. This unpredictability allows for abstract, playful, and dynamic works, especially if animations are added. Glitch art also fits the assignment challenge, since it emphasizes representing an artwork in a way that both copies and abstracts it through code.
 
-# Header 1
-## Heeader 2
-### Header 3
-
-
-**this is bold text**
-
-- Item 1
-- Item 2
-    - sub item
+### **Part 2**
+A useful coding technique for glitch art is glitch lines, where image data is disrupted by shifting horizontal or vertical pixel strips. In p5.js or Processing, this effect can be created using loadPixels() and updatePixels(), or by redrawing line segments at random offsets. This technique is powerful because it allows precise control over how chaotic or subtle the glitch appears. It would help me recreate the fragmented, distorted look of glitch art while still referencing the original image, aligning well with the assignment’s goal of representing an artwork through code.
--- sketch.js
+++ sketch.js
@@ -1,7 +1,127 @@
+//We need a variable to hold our image
+let img;
+
+//We will divide the image into segments
+let numSegments = 50;
+
+//We will store the segments in an array
+let segments = [];
+
+//lets add a variable to switch between drawing the image and the segments
+let drawSegments = true;
+
+//Let's make an object to hold the draw properties of the image
+let imageProperties = {
+  aspect: 0,
+  width: 0,
+  height: 0,
+  xOffset: 0,
+  yOffset:0
+}
+
+//And a variable for the canvas aspect ratio
+let canvasAspectRatio = 0;
+
+//lets load the image from disk
+function preload() {
+  img = loadImage('/assets/Mona_Lisa_by_Leonardo_da_Vinci_500_x_700.jpg');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  //We will make the canvas the same size as the image using its properties
+  createCanvas(windowWidth, windowHeight);
+
+  //let's calculate the aspect ratio of the image
+  imageProperties.aspect = img.width / img.height;
+
+  //and the aspect ratio of the canvas
+  canvasAspectRatio = width / height;
+  calculateImageProperties();
+  //We can use the width and height of the image to calculate the size of each segment
+  let segmentWidth = img.width / numSegments;
+  let segmentHeight = img.height / numSegments;
+  /*
+  Divide the original image into segments, we are going to use nested loops
+  */
+
+  for (let segYPos=0; segYPos<img.height; segYPos+=segmentHeight) {
+    //this is looping over the height
+    for (let segXPos=0; segXPos<img.width; segXPos+=segmentWidth) {
+      //We will use the x and y position to get the colour of the pixel from the image
+      //lets take it from the centre of the segment
+      let segmentColour = img.get(segXPos + segmentWidth / 2, segYPos + segmentHeight / 2);
+       let segment = new ImageSegment(segXPos,segYPos,segmentWidth,segmentHeight,segmentColour);
+       segments.push(segment);
+    }
+  }
 }
 
 function draw() {
-  background(220);
+  background(0);
+  if (drawSegments) {
+    //lets draw the segments to the canvas
+    for (const segment of segments) {
+      segment.draw();
+    }
+  } else {
+    //lets draw the image to the canvas
+    image(img, 0, 0);
+  }
+}
+function keyPressed() {
+  if (key == " ") {
+    //this is a neat trick to invert a boolean variable,
+    //it will always make it the opposite of what it was
+    drawSegments = !drawSegments;
+  }
+}
+
+function calculateImageProperties(){
+  //Check if the image is wider than the canvas
+  if (imageProperties.aspect > canvasAspectRatio){
+    //then we will draw the image to the width of the canvas
+    imageProperties.width = width;
+    //and calculate the height based on the aspect ratio
+    imageProperties.height = width / imageProperties.aspect;
+    /*Offset tells us how much we need to move things horizontally 
+    or vertically to fit the screen. */
+    imageProperties.yOffset = (height - imageProperties.height) / 2;
+    imageProperties.xOffset = 0
+
+  //Check if the image is longer than the canvas
+  } else if (imageProperties.aspect < canvasAspectRatio){ 
+    imageProperties.height = height;
+    imageProperties.width = height * imageProperties.aspect;
+    imageProperties.xOffset = (width - imageProperties.width) / 2;
+    imageProperties.yOffset = 0;
+
+  } else if (imageProperties.aspect == canvasAspectRatio) {
+    imageProperties.width = width;
+    imageProperties.height = height;
+    imageProperties.xOffset = 0;
+    imageProperties.yOffset = 0;
+
+  }
+}
+
+//Here is our class for the image segments, we start with the class keyword
+class ImageSegment {
+
+  constructor(srcImgSegXPosInPrm,srcImgSegYPosInPrm,srcImgSegWidthInPrm,srcImgSegHeightInPrm,srcImgSegColourInPrm) {
+    //these parameters are used to set the internal properties of an instance of the segment
+    //These parameters are named as imageSource as they are derived from the image we are using
+    this.srcImgSegXPos = srcImgSegXPosInPrm;
+    this.srcImgSegYPos = srcImgSegYPosInPrm;
+    this.srcImgSegWidth = srcImgSegWidthInPrm;
+    this.srcImgSegHeight = srcImgSegHeightInPrm;
+    this.srcImgSegColour = srcImgSegColourInPrm;
+  }
+
+  draw() {
+    //lets draw the segment to the canvas, for now we will draw it as an empty rectangle so we can see it
+    stroke(0);
+    fill(this.srcImgSegColour);
+    rect(this.srcImgSegXPos, this.srcImgSegYPos, this.srcImgSegWidth, this.srcImgSegHeight);
+    
+  }
 }
